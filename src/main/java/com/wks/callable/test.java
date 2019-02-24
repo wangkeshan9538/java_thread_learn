01453bcea1150dac9cf2ad27194d4feaf69e39e9
wangkeshan9538 <wangkeshan9538@gmail.com>
2019-02-24 20:43:15 +0800

    ''

--- src/main/java/com/wks/callable/test.java
+++ src/main/java/com/wks/callable/test.java
@@ -25,4 +25,16 @@ class Demo implements Callable<Integer>{
         Thread.sleep(2000);
         return 1;
     }
+
+    //看起来是个死循环 ，但其实因为i 一直累加 溢出之后 到-1 因为sout 很费时间，所以看起来像是进了死循环，
+    public static void main(String[] args) {
+        int i=0;
+        while(true){
+            i++;
+            //System.out.println(i);
+            if(i==-1){
+                break;
+            }
+        }
+     }
 }
